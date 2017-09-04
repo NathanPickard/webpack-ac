5ab08e0ccef0955c96aa1c515b9581eaee65cbfc
Nathan Pickard <nathan.pickard87@gmail.com>
2017-09-04 13:38:49 -0700

    file-loader with bundle.js

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "./build")
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /\.js$/,
+      //   use: "babel-loader"
+      // },
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     "style-loader",
+      //     "css-loader"
+      //   ]
+      // },
+      {
+        test: /\.jpeg$/,
+        use: ["file-loader"]
+        // use: [
+        //   {
+        //     loader: "url-loader", options: {
+        //       limit: 10000
+        //     }
+        //   }
+        // ]
+      }
+    ]
+  }
+};
